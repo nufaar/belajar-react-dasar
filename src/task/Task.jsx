0b3f075a15cefb16192165c5939ffdac97ce0418
nufaar <rozaq924@gmail.com>
2025-12-31 21:46:11 +0700

    feat: sharing state

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,3 +1,20 @@
+import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
+
 export default function Task() {
-  return <></>;
+  const [items, setItems] = useImmer([]);
+
+  function handleSubmit(item) {
+    setItems((draft) => {
+      draft.push(item);
+    });
+  }
+
+  return (
+    <div>
+      <TaskForm onSubmit={handleSubmit} />
+      <TaskList items={items} />
+    </div>
+  );
 }
---
+++ src/task/TaskForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function TaskForm({ onSubmit }) {
+  const [item, setItem] = useState("");
+
+  function handleChange(e) {
+    setItem(e.target.value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    onSubmit(item);
+    setItem("");
+  }
+
+  return (
+    <div>
+      <h1>Create Task</h1>
+      <form>
+        <input type="text" value={item} onChange={handleChange} />
+        <button onClick={handleClick}>Add</button>
+      </form>
+    </div>
+  );
+}
