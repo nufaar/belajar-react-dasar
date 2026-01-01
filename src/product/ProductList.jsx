a550c12a1af50b747d2cd3a219645540fa604593
nufaar <rozaq924@gmail.com>
2026-01-01 14:41:14 +0700

    feat: effect dependencies

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,27 +1,36 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./Product";
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
-  const loaded = useRef(false);
+  const [load, setLoad] = useState(false);
+
+  function handleClick() {
+    console.info("Handle click");
+    setLoad(true);
+  }
+
+  useEffect(() => {
+    console.info("call use effect with []");
+  }, []);
 
   useEffect(() => {
     console.info("call use effect");
-    if (loaded.current === false) {
+    if (load) {
       fetch("/products.json")
         .then((response) => response.json())
-        .then((data) => setProducts(data))
-        .then(() => (loaded.current = true));
+        .then((data) => setProducts(data));
     }
 
     return () => {
       console.info("Product list component unmounted");
     };
-  });
+  }, [load]);
 
   return (
     <>
       <h1>List Products</h1>
+      <button onClick={handleClick}>Load Product</button>
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
