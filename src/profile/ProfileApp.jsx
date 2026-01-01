1ab972adf99caf4c821e9e0559f762f9937ca4d4
nufaar <rozaq924@gmail.com>
2026-01-01 11:53:44 +0700

    feat: context and state

---
+++ src/profile/ProfileApp.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Profile from "./Profile";
+import ProfileAddress from "./ProfileAddress";
+import { ProfileContext } from "./ProfileContext";
+import ProfileForm from "./ProfileForm";
+
+export default function ProfileApp() {
+  const [name, setName] = useState("");
+  return (
+    <>
+      <ProfileContext.Provider value={name}>
+        <h1>Profile App</h1>
+        <ProfileForm name={name} setName={setName} />
+        <Profile />
+        <ProfileAddress />
+      </ProfileContext.Provider>
+    </>
+  );
+}
