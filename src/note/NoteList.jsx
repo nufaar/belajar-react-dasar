69bc6467a2ae49e49544292e35411245d04c5c7b
nufaar <rozaq924@gmail.com>
2026-01-01 15:53:40 +0700

    feat: custom hook

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useContext, useMemo, useRef, useState } from "react";
 import Note from "./Note";
 import { NotesContext } from "./NoteContext";
 
 export default function NoteList() {
   const notes = useContext(NotesContext);
+  const [search, setSearch] = useState("");
+  const serachInput = useRef(null);
+
+  const filteredNotes = useMemo(() => {
+    console.info("Filtering Notes");
+    return notes.filter((note) => note.text.includes(search));
+  }, [notes, search]);
+
+  function handleSearch() {
+    console.info("serach");
+    setSearch(serachInput.current.value);
+  }
+
   return (
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Note note={note} />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <input ref={serachInput} type="text" placeholder="Search" />
+      <button onClick={handleSearch}>Search</button>
+      <ul>
+        {filteredNotes.map((note) => (
+          <li key={note.id}>
+            <Note note={note} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
